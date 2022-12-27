5d5b27e738cd02cfdcf11347ad126eae74235a3f
Damir Nazirov <damirin500@gmail.com>
2022-12-27 09:31:58 +0300

    clear index and app

--- src/App.js
+++ src/App.js
@@ -1,53 +1,14 @@
 import React from 'react';
-import TodoItem from './features/todoItem/TodoItem';
 import TodoInput from './features/todoInput/TodoInput'
 import TodoList from './features/todoList/TodoList';
-import { useEffect } from 'react';
-
-import { store } from './app/store';
-// import {TodoInput} from './features/todoInput/TodoInput';
-// import {TodoItem} from './features/todoItem/TodoItem';
-// import {Filters} from './features/filters/Filters'
 
 import './App.css';
-
-// const {dispatch, getState, subscribe} = store;
-
-// const data = [
-//   {value: 'хлеб',
-//    id: 1,
-//    done: true 
-// },
-//   {value: 'молоко',
-//    id: 2,
-//    done: false  
-// }
-// ]
-
-// const data2 = getState().todos;
-// console.log(data2);
-
-
-
-
-
 
 function App() {
     return (
     <div className="App">
         <TodoInput/>
         <TodoList/>
-        {/* {data2.map(item => {
-          return (
-            <TodoItem
-              key={item.id}
-              value={item.value}
-              done={item.done} */}
-            {/* />
-          )
-        })} */}
-        {/* <Filters/> */}
-      {/* </header> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-import todosReducer from './reducers';
-import { addTodoItem, toggleDoneTodo, deleteTodo  } from './actions';
-
 import './index.css';
-
-
-const {dispatch} = store;
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
@@ -25,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
